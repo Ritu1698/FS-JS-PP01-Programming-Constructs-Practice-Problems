bf33dffd6e1cb8833475e276a2041c611d2c9362
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-10 17:39:33 +0530

    [Rituparna]Added: Logic To Calculate Wage For A Month

--- EmpWage.js
+++ EmpWage.js
@@ -1,12 +1,11 @@
 // Check if employee is Present or Absent
 const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-{
+function checkEmpPrestOrAbsent(empCheck, day) {
     if (empCheck == IS_ABSENT) {
-        console.log("Employee is ABSENT");
+        console.log("On " + day + " Employee is ABSENT");
     }
     else {
-        console.log("Employee is PRESENT");
+        console.log("On " + day + " Employee is PRESENT");
     }
 }
 
@@ -17,6 +16,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getEmpWorkingHours(empCheck) {
     switch (empCheck) {
@@ -28,6 +28,12 @@ function getEmpWorkingHours(empCheck) {
             return 0;
     }
 }
-let empHrs = getEmpWorkingHours(empCheck);
+// Calculate wages for a month
+let empHrs = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    checkEmpPrestOrAbsent(empCheck, day);
+    empHrs += getEmpWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total Hours: " + empHrs + " Total Wage: " + empWage);
