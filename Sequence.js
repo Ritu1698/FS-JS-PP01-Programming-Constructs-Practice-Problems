437d74b9c9628e4d4802f774f4afefa1d5e87bdb
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-10 17:56:53 +0530

    [Rituparna]Added: Random Function To Get A Dice Number(1-6)

--- Sequence.js
+++ Sequence.js
@@ -1,3 +1,7 @@
 //Random function to get single digit
 let rabdomNumber = Math.floor(Math.random() * 10);
-console.log(rabdomNumber);
+console.log("Random Number: " + rabdomNumber);
+
+//Random function to get dice value
+let diceValue = Math.ceil(Math.random() * 10) % 6;
+console.log("Number on Dice: " + diceValue);
