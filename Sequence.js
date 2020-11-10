5346753e789e86fd06d9d5a3b021f835bac0b810
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-10 18:05:39 +0530

    [Rituparna]Added: Random Function To Get Sum & Avg Of Five Random Numbers

--- Sequence.js
+++ Sequence.js
@@ -1,10 +1,13 @@
 //Random function to get single digit
-let rabdomNumber = Math.floor(Math.random() * 10);
-console.log("Random Number: " + rabdomNumber);
-
+{
+    let rabdomNumber = Math.floor(Math.random() * 10);
+    console.log("Random Number: " + rabdomNumber);
+}
 //Random function to get dice value
-let diceValue = Math.ceil(Math.random() * 10) % 6 + 1;
-console.log("Number on Dice: " + diceValue);
+{
+    let diceValue = Math.ceil(Math.random() * 10) % 6 + 1;
+    console.log("Number on Dice: " + diceValue);
+}
 
 //Add two random dice numbers
 {
@@ -12,4 +15,13 @@ console.log("Number on Dice: " + diceValue);
     let diceValueTwo = Math.floor(Math.random() * 6) + 1;
     let sum = diceValueOne + diceValueTwo;
     console.log("Random 2 Dices Sum: " + sum);
+}
+
+//Sum and Average of 5 Random 2 digit numbers
+{
+    let sum = 0;
+    for (let indexValue = 0; indexValue < 5; indexValue++) {
+        sum += Math.floor(Math.random() * 90) + 10;
+    }
+    console.log("Sum Value: " + sum + " Avg Value: " + sum / 5);
 }
