b77ae0fd3a30cf3ba68193f8a021675e9bc64a8d
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-10 19:44:53 +0530

    [Rituparna]Added: Check Prime Or Not

--- ForLoop.js
+++ ForLoop.js
@@ -17,3 +17,20 @@
     }
     console.log("Harmonic number " + harmonicNumber);
 }
+
+//Check Prime or Not
+{
+    const prompt = require('prompt-sync')();
+    let number = parseInt(prompt("Enter the number "));
+    let isPrimeNumber = true;
+    if (number == 1) {
+        isPrimeNumber = false;
+    }
+    for (let indexValue = 2; indexValue <= number / 2; indexValue++) {
+        if (number % indexValue == 0) {
+            isPrimeNumber = false;
+            break;
+        }
+    }
+    console.log(isPrimeNumber);
+}
