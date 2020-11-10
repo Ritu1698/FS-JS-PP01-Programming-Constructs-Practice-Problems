50640eb5f80391cc243571a3472ec91996dcd0b6
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-10 18:01:19 +0530

    [Rituparna]Added: Random Function To Get Sum Of Two Dices

--- Sequence.js
+++ Sequence.js
@@ -3,5 +3,13 @@ let rabdomNumber = Math.floor(Math.random() * 10);
 console.log("Random Number: " + rabdomNumber);
 
 //Random function to get dice value
-let diceValue = Math.ceil(Math.random() * 10) % 6;
+let diceValue = Math.ceil(Math.random() * 10) % 6 + 1;
 console.log("Number on Dice: " + diceValue);
+
+//Add two random dice numbers
+{
+    let diceValueOne = Math.floor(Math.random() * 6) + 1;
+    let diceValueTwo = Math.floor(Math.random() * 6) + 1;
+    let sum = diceValueOne + diceValueTwo;
+    console.log("Random 2 Dices Sum: " + sum);
+}
